769a1df2f259f124eab21e22e8a5f17a4bc82aec
viettranisnottaken <tranxuanviet1997@gmail.com>
2019-11-05 20:04:05 +0700

    wip

--- app/controllers/consumables_controller.rb
+++ app/controllers/consumables_controller.rb
@@ -1,4 +1,6 @@
 class ConsumablesController < ApplicationController
+  before_action :set_consumable, only: [:edit, :update, :show, :destroy]
+
   def index
     @consumables = Consumable.paginate(page: params[:page], per_page: 10)
   end
@@ -22,18 +24,27 @@ class ConsumablesController < ApplicationController
   end
   
   def edit
-  
+    respond_to do |format|
+      format.js
+    end
   end
   
   def update
-  
+    respond_to do |format|
+      format.js
+    end
   end
   
-  def show
-  
+  def show  
   end
   
   def destroy
   
+  end
+
+  private
+
+  def set_consumable
+    @consumable = Consumable.find(params[:id])
   end
 end
