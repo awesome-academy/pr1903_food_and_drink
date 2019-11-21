cde9fc7b9084959cf5bc587709b656c62aad9ba8
anhvan94 <nguyenanhvan1511@gmail.com>
2019-11-21 20:41:33 +0700

    feat(app):edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,57 @@
 class UsersController < ApplicationController
-  before_action :correct_user, only: [:edit, :update, :show]
- 
+  before_action :find_user, only: [:edit, :update, :show]
+  before_action :correct_user, only: [:edit, :update]
+
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.all.paginate(page: params[:page])
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "welcome to Foods and Drinks"
+      flash[:success] = t(".welcome to Foods and Drinks")
       redirect_to @user
     else
-      render :new
+      redirect_to @user
     end
   end
 
-  def show   
+  def show
   end
 
   def edit
   end
-  
+
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-  def correct_user
+  def find_user
     @user = User.find_by(id: params[:id])
     unless @user
      redirect_to root_path
      flash[:danger] = "User not found"
    end
-  end     
+  end
+
+  def correct_user
+    @user = User.find_by(id: params[:id])
+    unless @user == current_user
+      redirect_to(root_url)
+    end
+  end
 end
