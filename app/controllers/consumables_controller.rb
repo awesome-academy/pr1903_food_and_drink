5e481d73ed70000b66a70ba9223c60f38c1417db
anhvan94 <nguyenanhvan1511@gmail.com>
2019-12-09 18:56:38 +0700

    feat(webapp): show consumable

--- app/controllers/consumables_controller.rb
+++ app/controllers/consumables_controller.rb
@@ -3,15 +3,23 @@ class ConsumablesController < ApplicationController
   before_action :find_consumables, except: [:show]
 
   def index
+    @consumables = if params[:term]
+      Consumable.by_name(params[:term]).paginate(page: params[:page])
+    else
+      Consumable.all.paginate(page: params[:page])
+    end
   end
-  
+
   def new
     @consumable = Consumable.new
     respond_to do |format|
       format.js
     end
   end
-  
+
+  def show
+  end
+
   def create
     @consumable = Consumable.new(consumable_params)
     if @consumable.save
@@ -22,28 +30,25 @@ class ConsumablesController < ApplicationController
       render :index
     end
   end
-  
+
   def edit
     respond_to do |format|
       format.js
     end
   end
-  
+
   def update
     if @consumable.update_attributes(consumable_params)
       respond_to do |format|
         format.js
-      end      
+      end
     else
       render :index
       flash[:danger] = "consumable not created"
     end
 
   end
-  
-  def show  
-  end
-  
+
   def destroy
     @consumable.discard
     respond_to do |format|
@@ -60,7 +65,7 @@ class ConsumablesController < ApplicationController
   def find_consumable
     @consumable = Consumable.find_by(id: params[:id])
     unless @consumable
-      redirect_to root_path 
+      redirect_to root_path
       flash[:danger] = "Consumable not found"
     end
   end
